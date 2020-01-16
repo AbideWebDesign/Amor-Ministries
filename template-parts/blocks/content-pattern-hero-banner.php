f27755de279e104ee7a3bbe9c77d5a49c8d1053a
Josh Armentano <josh@abidewebdesign.com>
2020-01-16 15:06:49 -0800

    Added ACF json. Updates to pattern banner block.

--- template-parts/blocks/content-pattern-hero-banner.php
+++ template-parts/blocks/content-pattern-hero-banner.php
@@ -7,14 +7,26 @@
 
 // get group field (array)
 $banner_content = get_field('pattern_hero_banner_content');
+$type = $banner_content['type'];
 
+if ( $type == 'Form' ) {
+	
+	$row_class = 'row';
+	$col_class = 'col-lg-8 align-self-center';
+	
+} else {
+	
+	$row_class = 'row justify-content-center';
+	$col_class = 'col-md-8 col-lg-6 text-center';
+	
+}
 ?>
 
 <div class="container-pattern-hero">
 	<div class="pattern-hero-banner">
 		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-8 col-lg-6">
+			<div class="<?php echo $row_class; ?>">				
+				<div class="<?php echo $col_class; ?>">
 					<div class="hero-header">
 						<h2 class="mb-3"><?php echo $banner_content['title']; ?></h2>
 						<p class="lead mb-3"><?php echo $banner_content['text']; ?></p>
@@ -27,6 +39,25 @@ $banner_content = get_field('pattern_hero_banner_content');
 						
 					</div>
 				</div>
+				
+				<?php if ( $type == 'Form' ): ?>
+				
+					<div class="col-lg-4">
+						<div class="bg-light p-4">
+							
+							<?php if ( $banner_content['form_title'] ): ?>
+							
+								<h3 class="mb-3"><?php echo $banner_content['form_title']; ?></h3>
+								
+							<?php endif; ?>
+							
+							<?php echo do_shortcode('[gravityform id=" ' . $banner_content['form'] . '" title="false" description="false" ajax="true"]'); ?>
+					
+						</div>
+					</div>
+				
+				<?php endif; ?>
+								
 			</div>
 		</div>
 	</div>
