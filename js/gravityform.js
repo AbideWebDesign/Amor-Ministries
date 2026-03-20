94be8afb7dc557ca9bbf5f0f07952d3a2a81f939
Josh Armentano <josh@abidewebdesign.com>
2026-03-20 14:20:16 -0700

    no message

--- js/gravityform.js
+++ js/gravityform.js
@@ -21,21 +21,13 @@
 
         if ( $( this ).val() === 'Amor YP Champion' ) {
 
-            $priceInput.val( '$30.00' ).prop( 'disabled', true );
-
-            if ( ! $priceInput.next( '.ginput_amount_suffix' ).length ) {
-
-                $priceInput.after( '<span class="ginput_amount_suffix">/month</span>' );
-
-            }
+            $priceInput.val( '$30.00/month' ).prop( 'disabled', true );
 
             $monthlyCheckbox.prop( 'checked', true ).prop( 'disabled', true );
 
         } else {
 
             $priceInput.val( '' ).prop( 'disabled', false );
-
-            $priceInput.next( '.ginput_amount_suffix' ).remove();
 
             $monthlyCheckbox.prop( 'checked', false ).prop( 'disabled', false );
 
